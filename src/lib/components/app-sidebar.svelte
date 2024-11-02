6d6ed5540724bd92ab71a3161d470218ea3dd895
Henry <henry@gedert.de>
2024-11-02 17:21:32 +0100

    implemented navigation over sidebar

--- src/lib/components/app-sidebar.svelte
+++ src/lib/components/app-sidebar.svelte
@@ -1,149 +1,77 @@
-<script lang="ts" module>
-	// sample data
-	const data = {
-		versions: ['1.0.1', '1.1.0-alpha', '2.0.0-beta1'],
-		navMain: [
-			{
-				title: 'Getting Started',
-				url: '#',
-				items: [
-					{
-						title: 'Installation',
-						url: '#'
-					},
-					{
-						title: 'Project Structure',
-						url: '#'
-					}
-				]
-			},
-			{
-				title: 'Building Your Application',
-				url: '#',
-				items: [
-					{
-						title: 'Routing',
-						url: '#'
-					},
-					{
-						title: 'Data Fetching',
-						url: '#',
-						isActive: true
-					},
-					{
-						title: 'Rendering',
-						url: '#'
-					},
-					{
-						title: 'Caching',
-						url: '#'
-					},
-					{
-						title: 'Styling',
-						url: '#'
-					},
-					{
-						title: 'Optimizing',
-						url: '#'
-					},
-					{
-						title: 'Configuring',
-						url: '#'
-					},
-					{
-						title: 'Testing',
-						url: '#'
-					},
-					{
-						title: 'Authentication',
-						url: '#'
-					},
-					{
-						title: 'Deploying',
-						url: '#'
-					},
-					{
-						title: 'Upgrading',
-						url: '#'
-					},
-					{
-						title: 'Examples',
-						url: '#'
-					}
-				]
-			},
-			{
-				title: 'API Reference',
-				url: '#',
-				items: [
-					{
-						title: 'Components',
-						url: '#'
-					},
-					{
-						title: 'File Conventions',
-						url: '#'
-					},
-					{
-						title: 'Functions',
-						url: '#'
-					},
-					{
-						title: 'next.config.js Options',
-						url: '#'
-					},
-					{
-						title: 'CLI',
-						url: '#'
-					},
-					{
-						title: 'Edge Runtime',
-						url: '#'
-					}
-				]
-			},
-			{
-				title: 'Architecture',
-				url: '#',
-				items: [
-					{
-						title: 'Accessibility',
-						url: '#'
-					},
-					{
-						title: 'Fast Refresh',
-						url: '#'
-					},
-					{
-						title: 'Svelte Compiler',
-						url: '#'
-					},
-					{
-						title: 'Supported Browsers',
-						url: '#'
-					},
-					{
-						title: 'Rollup',
-						url: '#'
-					}
-				]
-			}
-		]
-	};
-</script>
-
 <script lang="ts">
-	import type { ComponentProps } from 'svelte';
 	import * as Sidebar from '$lib/components/ui/sidebar';
+	import {
+		ClipboardCheck,
+		ClipboardPlus,
+		SquareKanban,
+		UserRoundPlus,
+		UserRoundCog
+	} from 'lucide-svelte';
+	import { page, navigating } from '$app/stores';
 
-	let { ref = $bindable(null), ...restProps }: ComponentProps<typeof Sidebar.Root> = $props();
+	const nav = $state([
+		{
+			title: 'Scoreboard',
+			items: [
+				{
+					title: 'Scoreboard',
+					url: '/',
+					icon: SquareKanban,
+					isActive: true
+				},
+				{
+					title: 'Quests',
+					url: '/quests',
+					icon: ClipboardCheck
+				}
+			]
+		},
+		{
+			title: 'Management',
+			items: [
+				{
+					title: 'Get your user',
+					url: '/get-user',
+					icon: UserRoundPlus
+				},
+				{
+					title: 'Manage users',
+					url: '/manage-users',
+					icon: UserRoundCog
+				},
+				{
+					title: 'Manage quests',
+					url: '/manage-quests',
+					icon: ClipboardPlus
+				}
+			]
+		}
+	]);
+
+	navigating.subscribe((navigation) => {
+		if (navigation) {
+			activateRoute(navigation?.to?.route.id as string);
+		} else {
+			activateRoute($page.route.id as string);
+		}
+	});
+
+	function activateRoute(routeId: string) {
+		for (const group of nav) {
+			for (const route of group.items) {
+				if (route.url === routeId) {
+					route.isActive = true;
+				} else {
+					route.isActive = false;
+				}
+			}
+		}
+	}
 </script>
 
-<Sidebar.Root {...restProps} bind:ref>
-	<Sidebar.Header>Scoreboard</Sidebar.Header>
+<Sidebar.Root>
 	<Sidebar.Content>
-		<!-- We create a Sidebar.Group for each parent. -->
-		{#each data.navMain as group (group.title)}
+		{#each nav as group}
 			<Sidebar.Group>
 				<Sidebar.GroupLabel>{group.title}</Sidebar.GroupLabel>
 				<Sidebar.GroupContent>
@@ -151,7 +79,12 @@
 						{#each group.items as item (item.title)}
 							<Sidebar.MenuItem>
 								<Sidebar.MenuButton isActive={item.isActive}>
-									<a href={item.url}>{item.title}</a>
+									{#snippet child({ props })}
+										<a href={item.url} {...props}>
+											<item.icon />
+											<span>{item.title}</span>
+										</a>
+									{/snippet}
 								</Sidebar.MenuButton>
 							</Sidebar.MenuItem>
 						{/each}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,31 @@
 <script lang="ts">
 	import AppSidebar from '$lib/components/app-sidebar.svelte';
 	import LightSwitch from '$lib/components/light-switch.svelte';
-	import { Separator } from '$lib/components/ui/separator';
 	import * as Sidebar from '$lib/components/ui/sidebar';
 	import { ModeWatcher } from 'mode-watcher';
 	import '../app.css';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import { PanelLeftClose, PanelLeftOpen } from 'lucide-svelte';
 
 	let { children } = $props();
+	let open = $state(true);
 </script>
 
 <ModeWatcher />
-<Sidebar.Provider>
+<Sidebar.Provider bind:open>
 	<AppSidebar />
 	<Sidebar.Inset>
 		<header class="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-			<Sidebar.Trigger class="-ml-1" />
-			<Separator orientation="vertical" class="mr-2 h-4" />
+			<Button variant="ghost" onclick={() => (open = !open)}>
+				{#if open}
+					<PanelLeftClose></PanelLeftClose>
+					<span>Close Sidebar</span>
+				{:else}
+				<PanelLeftOpen></PanelLeftOpen>
+				<span>Open Sidebar</span>{/if}
+			</Button>
 			<div class="grow"></div>
 			<LightSwitch />
-
 		</header>
 		{@render children()}
 	</Sidebar.Inset>
