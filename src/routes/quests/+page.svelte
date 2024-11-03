47ffa5f13cd860f477f5c5c970a29911a5046694
Henry <henry@gedert.de>
2024-11-03 20:02:05 +0100

    added quest board

--- src/routes/quests/+page.svelte
+++ src/routes/quests/+page.svelte
@@ -1,1 +1,9 @@
-test
+<script>
+	import RegularQuestBoard from "./regular-quest-board.svelte";
+	import SpecialQuestBoard from "./special-quest-board.svelte";
+
+</script>
+<h1 class="title">Quest Board</h1>
+<RegularQuestBoard />
+<h1 class="title">Special Quest Board</h1>
+<SpecialQuestBoard />
---
+++ src/routes/quests/regular-quest-board.svelte
@@ -0,0 +1,30 @@
+<script>
+	import { Checkbox } from '$lib/components/ui/checkbox';
+	import { Label } from '$lib/components/ui/label';
+	import { Separator } from '$lib/components/ui/separator';
+</script>
+
+<div class="flex flex-row">
+	{#each [1, 2, 3] as num}
+		<div class="m-3 flex flex-col p-1 min-w-40">
+			<h2 class="mb-2">Dailys</h2>
+			<div>
+				<Checkbox id="task1{num}" />
+				<Label for="task1{num}">Task 1</Label>
+			</div>
+      <Separator class="my-1" />
+			<div>
+				<Checkbox id="task2{num}" />
+				<Label for="task2{num}">Task 2</Label>
+			</div>
+			<div>
+				<Checkbox id="task3{num}" />
+				<Label for="task3{num}">Task 3</Label>
+			</div>
+			<div>
+				<Checkbox id="task4{num}" />
+				<Label for="task4{num}">Task 4</Label>
+			</div>
+		</div>
+	{/each}
+</div>
